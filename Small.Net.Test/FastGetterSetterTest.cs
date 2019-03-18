cc4911d0ccb30e9af220f6d62a8a0e0a6c9eb991
Stephan <hougoul74@gmail.com>
2019-03-18 22:56:22 +0100

    Add Reflection test

---
+++ Small.Net.Test/FastGetterSetterTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Small.Net.Reflection;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+namespace Small.Net.Test
+{
+    [TestFixture]
+    public class FastGetterSetterTest
+    {
+        [Test]
+        public void IGetterSetterTest()
+        {
+            var objType = typeof(TestObject);
+            var properties = objType.GetPropertiesAccessor();
+            var obj = new TestObject();
+            properties["Prop1"].SetValue(obj, 10);
+            Assert.AreEqual(10, properties["PROP1"].GetValue(obj));
+
+            properties["prOp2"].SetValue(obj, 12L);
+            Assert.AreEqual(12L, properties["PROP2"].GetValue(obj));
+        }
+    }
+
+    public class TestObject
+    {
+        public int Prop1 { get; set; }
+
+        public long? Prop2 { get; set; }
+    } 
+}
---
+++ Small.Net/Reflection/ReflectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Reflection;
+using System.Linq;
+
+namespace Small.Net.Reflection
+{
+    public static class ReflectionExtensions
+    {
+        private static readonly ConcurrentDictionary<Type, Dictionary<string, IGetterSetter>> GetterSetterCache = new ConcurrentDictionary<Type, Dictionary<string, IGetterSetter>>();
+
+        public static IReadOnlyDictionary<string, IGetterSetter> GetPropertiesAccessor(this Type objType)
+        {
+            if (!GetterSetterCache.TryGetValue(objType, out var properties))
+            {
+                properties = new Dictionary<string, IGetterSetter>(StringComparer.OrdinalIgnoreCase);
+                var propertiesInfo = objType.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+                if (propertiesInfo != null)
+                {
+                    var getterSetterType = typeof(FastGetterSetter<>).MakeGenericType(objType);
+                    foreach(var propInfo in propertiesInfo)
+                    {
+                        var getterSetter = (IGetterSetter)Activator.CreateInstance(getterSetterType, propInfo);
+                        properties.Add(propInfo.Name, getterSetter);
+                    }
+                }
+                GetterSetterCache.TryAdd(objType, properties);
+            }
+            return properties;
+        }
+    }
+}
