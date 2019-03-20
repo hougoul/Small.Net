88bd1f3e94d0611b6717d069d6c2887827439079
Stephan <hougoul74@gmail.com>
2019-03-20 23:17:37 +0100

    Add CopyTo Extension

---
+++ Small.Net.Test/ObjectExtensionsTest.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using Small.Net.Extensions;
+
+namespace Small.Net.Test
+{
+    [TestFixture]
+    public class ObjectExtensionsTest
+    {
+        [Test]
+        public void CopyToTest()
+        {
+            var obj1 = new CopyObject1()
+            {
+                Prop2 = 5,
+                Prop3 = 10
+            };
+            var obj2 = obj1.CopyTo<CopyObject1, CopyObject1>();
+            Assert.AreEqual(obj1.Prop2, obj2.Prop2);
+            Assert.AreEqual(obj1.Prop3, obj2.Prop3);
+            var obj3 = obj1.CopyTo<CopyObject1, CopyObject2>();
+            Assert.AreEqual(10, obj3.Prop1);
+            Assert.AreEqual(5, obj3.Prop2);
+            Assert.AreEqual(obj1.Prop3, obj3.Prop3);
+        }
+    }
+
+    public class CopyObject1 : ICloneable
+    {
+        public int Prop1 { set => Prop2 = value % 10; }
+
+        public int Prop2 { get; set; }
+
+        public int Prop3 { get; set; }
+
+        public object Clone()
+        {
+            return new CopyObject1()
+            {
+                Prop2 = Prop2,
+                Prop3 = Prop3
+            };
+        }
+    }
+
+    public class CopyObject2
+    {
+        public int Prop1 { get; set; } = 10;
+
+        public int Prop2 { get; } = 5;
+
+        public int Prop3 { get; set; }
+    }
+
+}
---
+++ Small.Net/Extensions/ObjectExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Small.Net.Reflection;
+using System;
+using System.Linq;
+
+namespace Small.Net.Extensions
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Copies to.
+        /// </summary>
+        /// <typeparam name="T1">The type of the 1.</typeparam>
+        /// <typeparam name="T2">The type of the 2.</typeparam>
+        /// <param name="obj">The object.</param>
+        /// <returns></returns>
+        public static T2 CopyTo<T1,T2>(this T1 obj) where T1: class where T2 : class, new()
+        {
+            var t1Type = typeof(T1);
+            var t2Type = typeof(T2);
+            if (t1Type == t2Type && obj is ICloneable cloneable) return (T2)cloneable.Clone();
+            var copy = new T2();
+            var _getters = t1Type.GetPropertiesAccessor().Where(pair => pair.Value.HasGetter).ToDictionary(pair => pair.Key, pair => pair.Value ,StringComparer.OrdinalIgnoreCase);
+            var _setters = t2Type.GetPropertiesAccessor().Where(pair => pair.Value.HasSetter).ToDictionary(pair => pair.Key, pair => pair.Value, StringComparer.OrdinalIgnoreCase);
+            foreach(var getter in _getters)
+            {
+                if (_setters.ContainsKey(getter.Key))
+                    _setters[getter.Key].SetValue(copy, getter.Value.GetValue(obj));
+            }
+            return copy;
+        }
+    }
+}
