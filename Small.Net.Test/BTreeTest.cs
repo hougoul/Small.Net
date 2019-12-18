a0f758b2f983191747e19997a20bfa63cf1d2a2d
Stephan <hougoul74@gmail.com>
2019-12-18 11:14:46 +0100

    Add Unit Test

--- Small.Net.Test/BTreeTest.cs
+++ Small.Net.Test/BTreeTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using NUnit.Framework;
 using Small.Net.Collection;
 
@@ -16,17 +17,29 @@ namespace Small.Net.Test
         }
 
         private List<InternalTest> _withDuplicateKeys;
+        private List<InternalTest> _randomKeys;
 
         [OneTimeSetUp]
         public void Setup()
         {
             _withDuplicateKeys = new List<InternalTest>();
-            for (var i = 0; i < 10000; i++)
+            for (var i = 0; i < 1000000; i++)
             {
                 _withDuplicateKeys.Add(new InternalTest()
                 {
                     Id = i,
-                    IndexDate = DateTime.Today.AddDays(i % 75)
+                    IndexDate = DateTime.Today.AddDays(i % 550)
+                });
+            }
+
+            _randomKeys = new List<InternalTest>();
+            var random = new Random();
+            for (var i = 0; i < 1000000; i++)
+            {
+                _randomKeys.Add(new InternalTest()
+                {
+                    Id = 1,
+                    IndexDate = new DateTime(random.Next(1970, 2060), random.Next(1, 12), random.Next(1, 28))
                 });
             }
         }
@@ -35,12 +48,30 @@ namespace Small.Net.Test
         public void AddDuplicateTest()
         {
             var subject = new BpTree<InternalTest, DateTime>(obj => obj.IndexDate);
+            var watch = Stopwatch.StartNew();
             foreach (var test in _withDuplicateKeys)
             {
                 subject.Add(test);
             }
 
+            watch.Stop();
             Assert.AreEqual(_withDuplicateKeys.Count, subject.Count);
+            Console.WriteLine(watch.ElapsedMilliseconds);
+        }
+
+        [Test]
+        public void AddRandomTest()
+        {
+            var subject = new BpTree<InternalTest, DateTime>(obj => obj.IndexDate);
+            var watch = Stopwatch.StartNew();
+            foreach (var test in _randomKeys)
+            {
+                subject.Add(test);
+            }
+
+            watch.Stop();
+            Assert.AreEqual(_withDuplicateKeys.Count, subject.Count);
+            Console.WriteLine(watch.ElapsedMilliseconds);
         }
     }
 }
