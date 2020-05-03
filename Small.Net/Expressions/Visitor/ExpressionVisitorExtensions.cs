de311149bba5e460d5a3d1b45e21f3b0aac9f1dc
Stephan <hougoul74@gmail.com>
2020-05-03 23:58:54 +0200

    Add Binary and Unary Type expression

--- Small.Net/Expressions/Visitor/ExpressionVisitorExtensions.cs
+++ Small.Net/Expressions/Visitor/ExpressionVisitorExtensions.cs
@@ -32,16 +32,25 @@ namespace Small.Net.Expressions.Visitor
                 case ExpressionType.LessThan:
                 case ExpressionType.LessThanOrEqual:
                 case ExpressionType.Coalesce:
+                case ExpressionType.AddAssign:
+                case ExpressionType.AddAssignChecked:
+                case ExpressionType.AndAssign:
+                case ExpressionType.Assign:
+                case ExpressionType.DivideAssign:
+                case ExpressionType.ExclusiveOrAssign:
+                case ExpressionType.LeftShiftAssign:
+                case ExpressionType.ModuloAssign:
+                case ExpressionType.MultiplyAssign:
+                case ExpressionType.MultiplyAssignChecked:
+                case ExpressionType.OrAssign:
+                case ExpressionType.PowerAssign:
+                case ExpressionType.RightShiftAssign:
+                case ExpressionType.SubtractAssign:
+                case ExpressionType.SubtractAssignChecked:
                     return new BinaryVisitor<TNodeOutput>((BinaryExpression) node);
                 case ExpressionType.ArrayIndex:
                     if (node is BinaryExpression binNode) return new BinaryVisitor<TNodeOutput>(binNode);
                     return new MethodCallVisitor<TNodeOutput>((MethodCallExpression) node);
-                case ExpressionType.AddAssign:
-                    break;
-                case ExpressionType.AddAssignChecked:
-                    break;
-                case ExpressionType.AndAssign:
-                    break;
                 case ExpressionType.ArrayLength:
                 case ExpressionType.Convert:
                 case ExpressionType.ConvertChecked:
@@ -51,9 +60,18 @@ namespace Small.Net.Expressions.Visitor
                 case ExpressionType.Quote:
                 case ExpressionType.TypeAs:
                 case ExpressionType.UnaryPlus:
+                case ExpressionType.Decrement:
+                case ExpressionType.Increment:
+                case ExpressionType.IsFalse:
+                case ExpressionType.IsTrue:
+                case ExpressionType.OnesComplement:
+                case ExpressionType.PostDecrementAssign:
+                case ExpressionType.PostIncrementAssign:
+                case ExpressionType.PreDecrementAssign:
+                case ExpressionType.PreIncrementAssign:
+                case ExpressionType.Throw:
+                case ExpressionType.Unbox:
                     return new UnaryVisitor<TNodeOutput>((UnaryExpression) node);
-                case ExpressionType.Assign:
-                    break;
                 case ExpressionType.Block:
                     break;
                 case ExpressionType.Call:
@@ -64,36 +82,22 @@ namespace Small.Net.Expressions.Visitor
                     return new ConstantVisitor<TNodeOutput>((ConstantExpression) node);
                 case ExpressionType.DebugInfo:
                     break;
-                case ExpressionType.Decrement:
-                    break;
                 case ExpressionType.Default:
                     break;
-                case ExpressionType.DivideAssign:
-                    break;
                 case ExpressionType.Dynamic:
-                    break;
-                case ExpressionType.ExclusiveOrAssign:
                     break;
                 case ExpressionType.Extension:
                     break;
                 case ExpressionType.Goto:
                     break;
-                case ExpressionType.Increment:
-                    break;
                 case ExpressionType.Index:
                     break;
                 case ExpressionType.Invoke:
-                    break;
-                case ExpressionType.IsFalse:
-                    break;
-                case ExpressionType.IsTrue:
                     break;
                 case ExpressionType.Label:
                     break;
                 case ExpressionType.Lambda:
                     return new LambdaVisitor<TNodeOutput>((LambdaExpression) node);
-                case ExpressionType.LeftShiftAssign:
-                    break;
                 case ExpressionType.ListInit:
                     break;
                 case ExpressionType.Loop:
@@ -102,53 +106,23 @@ namespace Small.Net.Expressions.Visitor
                     break;
                 case ExpressionType.MemberInit:
                     break;
-                case ExpressionType.ModuloAssign:
-                    break;
-                case ExpressionType.MultiplyAssign:
-                    break;
-                case ExpressionType.MultiplyAssignChecked:
-                    break;
                 case ExpressionType.New:
                     break;
                 case ExpressionType.NewArrayBounds:
                     break;
                 case ExpressionType.NewArrayInit:
                     break;
-                case ExpressionType.OnesComplement:
-                    break;
-                case ExpressionType.OrAssign:
-                    break;
                 case ExpressionType.Parameter:
                     return new ParameterVisitor<TNodeOutput>((ParameterExpression) node);
-                case ExpressionType.PostDecrementAssign:
-                    break;
-                case ExpressionType.PostIncrementAssign:
-                    break;
-                case ExpressionType.PowerAssign:
-                    break;
-                case ExpressionType.PreDecrementAssign:
-                    break;
-                case ExpressionType.PreIncrementAssign:
-                    break;
-                case ExpressionType.RightShiftAssign:
-                    break;
                 case ExpressionType.RuntimeVariables:
                     break;
-                case ExpressionType.SubtractAssign:
-                    break;
-                case ExpressionType.SubtractAssignChecked:
-                    break;
                 case ExpressionType.Switch:
-                    break;
-                case ExpressionType.Throw:
                     break;
                 case ExpressionType.Try:
                     break;
                 case ExpressionType.TypeEqual:
                     break;
                 case ExpressionType.TypeIs:
-                    break;
-                case ExpressionType.Unbox:
                     break;
             }
 
