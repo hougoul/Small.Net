c52756d13ed61064cad79e0634002912ac1198bc
Stephan <hougoul74@gmail.com>
2020-06-14 19:20:08 +0200

    add Wpf application

---
+++ WorldEditor/Views/ShellView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace WorldEditor.Views
+{
+    public partial class ShellView : Window
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+        }
+    }
+}
