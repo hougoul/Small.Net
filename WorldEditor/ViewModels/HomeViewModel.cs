05a61237b189ed358cf97282eeff46b3ff6aa700
Stephan <hougoul74@gmail.com>
2020-06-28 19:23:44 +0200

    Use MVVM for loading...

--- WorldEditor/ViewModels/HomeViewModel.cs
+++ WorldEditor/ViewModels/HomeViewModel.cs
@@ -1,6 +1,10 @@
 using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 using Caliburn.Micro;
 using Small.Net.Graphic.Core;
+using WorldEditor.Entities;
 
 namespace WorldEditor.ViewModels
 {
@@ -17,5 +21,29 @@ namespace WorldEditor.ViewModels
         }
 
         public IEngine Engine { get; private set; }
+
+        public async void OnLoaded(SwapChainPanel screenView)
+        {
+            var handle = new UwpWindowHandle(screenView);
+            if (Engine.Initialise(handle))
+            {
+                Engine.Render();
+                return;
+            }
+
+            var messageDialog = new MessageDialog("no Direct X engine");
+            await messageDialog.ShowAsync();
+        }
+
+        public void OnResize(SizeChangedEventArgs e)
+        {
+            if (!Engine.IsInitialised)
+            {
+                return;
+            }
+
+            Engine.Resize((int) e.NewSize.Width, (int) e.NewSize.Height);
+            Engine.Render();
+        }
     }
 }
--- WorldEditor/Views/HomeView.xaml.cs
+++ WorldEditor/Views/HomeView.xaml.cs
@@ -1,9 +1,4 @@
 ﻿using Windows.UI.Xaml.Controls;
-using System;
-using Windows.UI.Popups;
-using Windows.UI.Xaml;
-using WorldEditor.Entities;
-using WorldEditor.ViewModels;
 
 
 namespace WorldEditor.Views
@@ -16,33 +11,6 @@ namespace WorldEditor.Views
         public HomeView()
         {
             this.InitializeComponent();
-        }
-
-        private async void Grid_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-            var handle = new UwpWindowHandle(DirectXPanel);
-            if (DataContext is HomeViewModel model)
-            {
-                if (model.Engine.Initialise(handle))
-                {
-                    model.Engine.Render();
-                    return;
-                }
-            }
-
-            var messageDialog = new MessageDialog("no Direct X engine");
-            await messageDialog.ShowAsync();
-        }
-
-        private void DirectXPanel_OnSizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            if (!(DataContext is HomeViewModel model) || !model.Engine.IsInitialised)
-            {
-                return;
-            }
-
-            model.Engine.Resize((int) e.NewSize.Width, (int) e.NewSize.Height);
-            model.Engine.Render();
         }
     }
 }
